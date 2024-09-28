d221684e58c7caa0d43c600b2b681f62343e988d
barslomski <108878607+barslomski@users.noreply.github.com>
2024-09-28 23:34:57 +0200

    latest

--- src/components/widgets/WalletWidget.jsx
+++ src/components/widgets/WalletWidget.jsx
@@ -1,23 +1,50 @@
-import { React, useState } from 'react'
+import { React, useEffect, useState } from 'react'
+import { generateClient } from 'aws-amplify/api';
+import { etherscanLambda } from '../../graphql/queries';
+
+const client = generateClient();
 
 function WalletWidget(props) {
     const [walletAddress, setWalletAddress] = useState('');
 
-    function searchWalletInDatabase(walletAddress) {
-        if (walletAddress) {
-            console.log("Searching for wallet: " + walletAddress);
+    async function scanWalletWithEtherscan(address) {
+        try {
+            const response = await client.graphql({
+                query: etherscanLambda,
+                variables: { address: address },
+                authMode: 'apiKey'
+            })
+
+            console.log(response)
+        }
+        catch (e) {
+            console.log(e);
         }
     }
 
+    useEffect(() => {
+        scanWalletWithEtherscan(props.walletInfo.id);
+    })
+
     return (
-        <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
-            <input
-                style={{ textAlign: 'center' }}
-                value={walletAddress}
-                onChange={e => setWalletAddress(e.target.value)}
-            />
-            <button onClick={() => searchWalletInDatabase(walletAddress)}>Search</button>
-        </div>
+        <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', width: '100%', height: '100%' }}>
+            <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center' }}>
+                <p style={{ border: '1px solid black', padding: 10, }}>
+                    {props.walletInfo.name}
+                </p>
+                <hr style={{
+                    width: '30vw',
+                    marginLeft: '1vw',
+                    marginRight: '1vw',
+                }} />
+                <p style={{ border: '1px solid black', padding: 10, }}>
+                    {"ocena rzet"}
+                </p>
+
+            </div>
+
+
+        </div >
     )
 }
 
